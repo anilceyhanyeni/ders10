18a7ac4d61bda1a035044f2664f124eb2c689afd
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-03-19 18:12:35 +0300

    feat: state giriş yapıldı.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import C1 from "./C1"
+
+function App(ozellikler) {
+    let [sayi, sayiGuncelle] = useState(0)
+
+    console.log("App componenti render oldu!")
+
+    let sayiArttır = ()=>{
+        sayiGuncelle( onceki => onceki + 1  )
+    }
+
+    return (
+        <>
+            <p>Merhaba, ben APP1 {ozellikler.renk}</p>
+            <p>Sayı: {sayi}  <button onClick={ sayiArttır }>Arttır</button>   </p>
+            <C1 isimler={["Ahmet", "Esra"]} />
+        </>
+    )
+}
+
+export default App
---
+++ src/C1.jsx
@@ -0,0 +1,8 @@
+const C1 = () => {
+    console.log("C1 componenti render oldu")
+    return (
+        <p>Ben C1 componentiyim</p>
+    )
+}
+
+export default C1
